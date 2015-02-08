569e0a6b89c2c5563a2f14eb4b76111706b5c0c9
YAAB <yaab@server.local>
2015-02-08 16:16:04 +0200

    git

---
+++ Gitt.h
@@ -0,0 +1,15 @@
+//
+//  Gitt.h
+//  GitUse
+//
+//  Created by YAAB on 2/8/15.
+//  Copyright (c) 2015 YAAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Gitt : NSObject
+-(void)commit;
+
+
+@end
