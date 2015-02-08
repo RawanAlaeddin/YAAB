3c1d0ac014677b8224c1959fd8b9c75b7f90cc04
YAAB <yaab@server.local>
2015-02-08 11:51:27 +0200

    Initial Commit

---
+++ GitUse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUse
+//
+//  Created by YAAB on 2/8/15.
+//  Copyright (c) 2015 YAAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
