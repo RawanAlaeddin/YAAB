da8a1955b574449bcc6e73f0516769e2fcb96680
YAAB <yaab@server.local>
2015-02-08 16:12:29 +0200

    com1

---
+++ GitUse/Git2.h
@@ -0,0 +1,15 @@
+//
+//  Git2.h
+//  GitUse
+//
+//  Created by YAAB on 2/8/15.
+//  Copyright (c) 2015 YAAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Git2 : NSObject
+-(void)git;
+
+
+@end
